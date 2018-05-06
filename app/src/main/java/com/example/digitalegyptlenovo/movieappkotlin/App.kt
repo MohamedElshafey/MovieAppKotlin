a88b84fde275a17ff194e9276dfe8330fdbe5510
Mohamed El-Shafey <mohamedes335@gmail.com>
2018-05-06 14:42:30 +0200

    Add Leak Canary

--- app/src/main/java/com/example/digitalegyptlenovo/movieappkotlin/App.kt
+++ app/src/main/java/com/example/digitalegyptlenovo/movieappkotlin/App.kt
@@ -5,6 +5,7 @@ import com.example.digitalegyptlenovo.movieappkotlin.dagger.component.DaggerNetw
 import com.example.digitalegyptlenovo.movieappkotlin.dagger.component.NetworkComponent
 import com.example.digitalegyptlenovo.movieappkotlin.dagger.module.NetworkModule
 import com.example.digitalegyptlenovo.movieappkotlin.webservice.MovieDbAPiConst
+import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by Mohamed Elshafey on 4/16/2018.
@@ -14,6 +15,13 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         networkComponent = DaggerNetworkComponent.builder()
                 .networkModule(NetworkModule(MovieDbAPiConst.baseUrl))
